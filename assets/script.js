0fc8d83c60f81f6581a2a0d5a235fd3a983d801a
Ashley Hodge <ashhodge@comcast.net>
2022-01-23 16:24:49 -0500

    add color based on time

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,43 @@
-var today = moment();
-
-
+// display current date in header
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+// save text for each time block to local storage
 $(document).ready(function(){
-    $("#currentDay").text(moment().format("dddd, MMMM Do")); 
     $(".saveBtn").on("click", function(){
-        var plannerText = $(this).siblings(".description").val();
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-        localStorage.setItem(time, plannerText);
-
+        localStorage.setItem(time, text);
     }); 
-});
+    
+// call data from local storage for each time block
+    $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+ 
+    // track time and add color to show past, present, and future
+    function timeChange(){
+        var currentHour = moment().hour(); 
+        
+        $(".time-block").each(function(){
+            var idTime = parseInt($(this).attr("id"));
 
+            if (idTime < currentHour){
+                $(this).addClass("past");
+                
+            } else if (idTime === currentHour){
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            } else {$(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+        })
+    }
+    timeChange();
+});
